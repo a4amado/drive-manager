1a67b5c5195dd5066182ba26ade58352c0bf8549
Ahmad Adel <a4addel@gmail.com>
2022-09-29 21:03:54 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,28 @@
-import 'react-virtualized/styles.css';
-import '../styles/index.css';
-import 'antd/dist/antd.css';
+import "../styles/index.css";
+import "antd/dist/antd.css";
 import "nprogress/nprogress.css";
 
 import React from "react";
-import { SessionProvider } from "next-auth/react"
-import  NProgress from "nprogress";
-import Router from 'next/router';
+import { SessionProvider } from "next-auth/react";
+import NProgress from "nprogress";
+import Router from "next/router";
 NProgress.configure({
-    showSpinner: true
-})
+  showSpinner: true,
+});
 Router.events.on("routeChangeStart", () => {
-    NProgress.start()
+  NProgress.start();
 });
 
 Router.events.on("routeChangeComplete", () => {
-    NProgress.done()
+  NProgress.done();
 });
 
-
 const MyApp = ({ Component, pageProps }: any) => {
-    return <SessionProvider session={pageProps?.session}> 
-    
-    <Component {...pageProps}/>
-    
+  return (
+    <SessionProvider session={pageProps?.session}>
+      <Component {...pageProps} />
     </SessionProvider>
+  );
 };
-
-
 
 export default MyApp;
